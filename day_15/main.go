02716377dce487ffeb12a197836254de6b302632
Alexandr SARANDI <sarandi.alex@gmail.com>
2020-12-15 00:12:54 -0800

    remove image.Point

--- day_15/main.go
+++ day_15/main.go
@@ -3,28 +3,27 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"io/ioutil"
 	"strconv"
 	"strings"
 )
 
-var puzzle = map[int]image.Point{}
+var puzzle = map[int]int{}
 
 func run(n int) (res int) {
-	p := map[int]image.Point{}
+	p := map[int]int{}
 	for i, v := range puzzle {
 		p[i] = v
 	}
-	v, seen := image.Point{}, false
+	v, seen := 0, false
 	for i := len(p) + 1; i <= n; i++ {
 		if !seen {
 			res = 0
 		} else {
-			res = i - 1 - v.X
+			res = i - 1 - v
 		}
 		v, seen = p[res]
-		p[res] = image.Point{i, v.X}
+		p[res] = i
 	}
 	return
 }
@@ -34,7 +33,7 @@ func main() {
 	input := strings.TrimSpace(string(data))
 	for i, v := range strings.Split(input, ",") {
 		n, _ := strconv.Atoi(v)
-		puzzle[n] = image.Point{i + 1, 0}
+		puzzle[n] = i + 1
 	}
 	fmt.Println("part 1:", run(2020))
 	fmt.Println("part 2:", run(30000000))
