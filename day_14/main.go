8be5c27331a7d67e7a85eea844be64cd79a2b298
Alexandr SARANDI <sarandi.alex@gmail.com>
2020-12-15 20:50:56 -0800

    put both solutions into one func

--- day_14/main.go
+++ day_14/main.go
@@ -10,77 +10,68 @@ import (
 )
 
 type item struct {
-	s         *string
+	mask      string
 	addr, val uint64
 }
 
-var (
-	puzzle = []item{}
-)
+var puzzle = []item{}
 
-func part1() (res uint64) {
-	mem := map[uint64]uint64{}
+func solve() (uint64, uint64) {
+	var (
+		mem1         = map[uint64]uint64{}
+		mem2         = map[uint64]uint64{}
+		part1, part2 uint64
+	)
 	for _, item := range puzzle {
-		s, addr, val := item.s, item.addr, item.val
-		for i := 0; i < 36; i++ {
-			switch (*s)[36-1-i] {
-			case '1':
-				val |= 1 << i
-			case '0':
-				val &= ^(1 << i)
-			}
-		}
-		mem[addr] = val
-	}
-	for _, val := range mem {
-		res += val
-	}
-	return
-}
-
-func part2() (res uint64) {
-	mem := map[uint64]uint64{}
-	for _, item := range puzzle {
-		s, val := item.s, item.val
-		xct := uint64(strings.Count(*s, "X"))
+		xct := uint64(strings.Count(item.mask, "X"))
 		for j := uint64(0); j < 1<<xct; j++ {
-			addr, xpos := item.addr, uint64(0)
-			for i := 0; i < 36; i++ {
-				switch (*s)[36-1-i] {
+			val1, val2 := item.val, item.val
+			addr1, addr2 := item.addr, item.addr
+			var i, xpos uint64
+			for ; i < 36; i++ {
+				switch item.mask[36-1-i] {
+				case '0':
+					val1 &= ^(1 << i)
 				case '1':
-					addr |= (1 << i)
+					val1 |= (1 << i)
+					addr2 |= (1 << i)
 				case 'X':
-					addr &= ^(1 << i)
-					addr |= ((j >> xpos) & 1) << i
+					addr2 &= ^(1 << i)
+					addr2 |= ((j >> xpos) & 1) << i
 					xpos++
 				}
 			}
-			mem[addr] = val
+			mem1[addr1] = val1
+			mem2[addr2] = val2
 		}
 	}
-	for _, val := range mem {
-		res += val
+	for _, val := range mem1 {
+		part1 += val
 	}
-	return
+	for _, val := range mem2 {
+		part2 += val
+	}
+	return part1, part2
 }
 
 func main() {
 	var (
-		mask     []string
 		reMask   = regexp.MustCompile(`^mask = (\w+)$`)
 		reMemset = regexp.MustCompile(`^mem\[(\d+)\] = (\d+)$`)
+		mask     string
 	)
 	data, _ := ioutil.ReadFile("input.txt")
 	for _, line := range strings.Split(string(data), "\n") {
 		if reMask.MatchString(line) {
-			mask = reMask.FindStringSubmatch(line)
+			mask = reMask.FindStringSubmatch(line)[1]
 		} else if reMemset.MatchString(line) {
 			memset := reMemset.FindStringSubmatch(line)
 			addr, _ := strconv.ParseUint(memset[1], 10, 64)
 			val, _ := strconv.ParseUint(memset[2], 10, 64)
-			puzzle = append(puzzle, item{s: &mask[1], addr: addr, val: val})
+			puzzle = append(puzzle, item{mask: mask, addr: addr, val: val})
 		}
 	}
-	fmt.Println("part 1:", part1())
-	fmt.Println("part 2:", part2())
+	part1, part2 := solve()
+	fmt.Println("part 1:", part1)
+	fmt.Println("part 2:", part2)
 }
