a3080c1648ea4a790fb8cc4806df10f0f78f7ef3
Alexandr SARANDI <sarandi.alex@gmail.com>
2020-12-18 00:50:42 -0800

    cosmetics

--- day_18/main.go
+++ day_18/main.go
@@ -17,33 +17,37 @@ func calc(s ...string) string {
 	return fmt.Sprintf("%d", a*b)
 }
 
-func eval(s string, f bool) int {
+func eval(s string, prio bool) int {
 	tok := strings.Fields(s)
 	if len(tok) == 1 {
 		res, _ := strconv.Atoi(tok[0])
 		return res
 	}
-	f = f && strings.Index(s, "+") != -1
-	for i, v := range tok {
-		if !(v == "+" || v == "*") ||
-			(f && v != "+") {
-			continue
+	prio = prio && strings.Index(s, "+") != -1
+	i := 1
+	for i = range tok {
+		if tok[i] == "+" || tok[i] == "*" {
+			if prio && tok[i] == "+" {
+				break
+			} else if !prio {
+				break
+			}
 		}
-		repl := calc(tok[i-1 : i+2]...)
-		newTok := append(append(tok[:i-1], repl), tok[i+2:]...)
-		return eval(strings.Join(newTok, " "), f)
 	}
-	return 0
+	repl := calc(tok[i-1 : i+2]...)
+	newTok := append(append(tok[:i-1], repl), tok[i+2:]...)
+	return eval(strings.Join(newTok, " "), prio)
 }
 
-func subexpr(s string, f bool) int {
-	if i := strings.LastIndex(s, "("); i != -1 {
-		j := strings.Index(s[i:], ")") + i
-		e := eval(s[i+1:j], f)
-		d := fmt.Sprintf("%d", e)
-		return subexpr(s[:i]+d+s[j+1:], f)
+func subexpr(s string, prio bool) int {
+	i := strings.LastIndex(s, "(")
+	if i == -1 {
+		return eval(s, prio)
 	}
-	return eval(s, f)
+	j := strings.Index(s[i:], ")") + i
+	sub := eval(s[i+1:j], prio)
+	repl := fmt.Sprintf("%d", sub)
+	return subexpr(s[:i]+repl+s[j+1:], prio)
 }
 
 func main() {
